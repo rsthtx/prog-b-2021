c1a257bacf99766302f223d12ab658a0eebe532f
Rasmus Stougaard <rst@eucnordvest.dk>
2021-09-16 10:00:44 +0200

    Add bouncing ball starter demo

--- bouncing-ball-demo/sketch.js
+++ bouncing-ball-demo/sketch.js
@@ -1,10 +1,15 @@
+let xPos = 30;
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
   background(220);
-  
-  circle(200,150,70)
+  let speed = 1
+
+  circle(xPos,150,50)
+
+  xPos = xPos +(3 * speed);
 
 }
