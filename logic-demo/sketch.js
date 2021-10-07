8fe129c2bde71bcbb197096893cc0a29308bc5fd
Rasmus Stougaard <rst@eucnordvest.dk>
2021-10-07 08:46:52 +0200

    Add logic demo

--- logic-demo/sketch.js
+++ logic-demo/sketch.js
@@ -8,7 +8,24 @@ function draw() {
   let x = mouseX
   let y = mouseY
   let diameter = 80
+
+  let horizontalDivider = width / 2
+  let verticalDivider = height / 2
+
   
+  let isLeft = x < horizontalDivider
+  let isAbove = y < verticalDivider
+  
+  if ((isLeft && isAbove) || (!isLeft && !isAbove)) {
+    fill('red')
+  } else {
+    fill('white')
+  }
+
+  // divider lines
+  line(horizontalDivider, 0, horizontalDivider, height)
+  line(0, verticalDivider, width, verticalDivider)
+
   circle(x, y, diameter)
 
 }
