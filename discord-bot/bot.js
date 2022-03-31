ec48e80898451eb8afb41e27943a2b00b2cf8570
Rasmus Stougaard <rst@eucnordvest.dk>
2022-03-31 13:26:34 +0200

    WIP. reading messages from guild

--- discord-bot/bot.js
+++ discord-bot/bot.js
@@ -1,1 +1,23 @@
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+require('dotenv').config()
+
+const TOKEN = process.env.TOKEN
+
 console.log("Beep Beep 🤖")
+
+client.on('ready', readyDiscord)
+
+function readyDiscord() {
+  console.log('logged in 👍')
+  console.log(`Logged in as ${client.user.tag}!`);
+}
+
+client.on('messageCreate', messageCreated)
+
+function messageCreated(msg) {
+  console.log(msg.content)
+}
+
+client.login(TOKEN);
