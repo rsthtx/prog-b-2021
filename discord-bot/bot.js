d7e5a10534699000e079ca7d78a88fc253871596
Rasmus Stougaard <rst@eucnordvest.dk>
2022-04-07 10:06:16 +0200

    WIP. discord bot

--- discord-bot/bot.js
+++ discord-bot/bot.js
@@ -1,5 +1,11 @@
 const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const config = {
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+  ]
+}
+const client = new Client(config);
 
 require('dotenv').config()
 
@@ -10,14 +16,30 @@ console.log("Beep Beep 🤖")
 client.on('ready', readyDiscord)
 
 function readyDiscord() {
-  console.log('logged in 👍')
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Logged in as ${client.user.tag} 👍!`);
 }
 
 client.on('messageCreate', messageCreated)
 
 function messageCreated(msg) {
-  console.log(msg.content)
+  const s = `User ${msg.author.username} wrote: ${msg.content}`
+  console.log(s)
+
+  const botTestingChannel = process.env.CHANNEL_ID
+
+  const replies = [
+    '🤖👌😍',
+    'hej',
+    'Velkommen',
+    'Er der snart pause?',
+  ]
+
+  if (msg.channel.id === botTestingChannel && msg.content === 'mojn') {
+
+    const index = Math.floor(Math.random() * replies.length)
+    const reply = replies[index]
+    msg.channel.send(reply)
+  }
 }
 
 client.login(TOKEN);
