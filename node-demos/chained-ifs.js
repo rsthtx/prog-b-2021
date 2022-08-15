a8f97ffc98b27ea17038b5394556687128c2251c
Rasmus Stougaard <rst@eucnordvest.dk>
2022-08-15 12:17:45 +0200

    Add chained ifs

---
+++ node-demos/chained-ifs.js
@@ -0,0 +1,18 @@
+
+let temp = 21;
+
+if(18 > temp){
+  console.log("for koldt til at arbejde")
+} else if( 21 > temp){
+  if( 21 == temp){
+    console.log("det dejligt")
+  }
+  
+  console.log("perfekt pause temperatur")
+} else {
+  console.log("for vormt til at arbejde")
+}
+
+if( 19 == temp){
+  console.log("det er lunt")
+}
